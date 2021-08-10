f4fa43abfc61120c2969d6600ea4af4c8b8b61c4
RasmusMS <rasmus5151@gmail.com>
2021-08-10 09:06:09 +0200

    Added ifndef to my object file for TCP connection

--- connection/connection.h
+++ connection/connection.h
@@ -1,5 +1,5 @@
-//#ifndef ETH_HANDLING_H
-//#define ETH_HANDLING_H
+#ifndef ETH_HANDLING_H
+#define ETH_HANDLING_H
 
 #include "mbed.h"
 #include "EthernetInterface.h"
@@ -46,4 +46,4 @@ class myConnection {
         }
 };
 
-//#endif
+#endif
